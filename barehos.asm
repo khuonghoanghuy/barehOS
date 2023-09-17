53fefd61c4bd6e08ca7d4234fd1e494598015587
ThatPeople <63274635+khuonghoanghuy@users.noreply.github.com>
2023-09-17 10:03:47 +0700

    simple bootloader thing

--- barehos.asm
+++ barehos.asm
@@ -1,2 +1,14 @@
 [bits 16]
 [org 0x7C00]
+start:
+    cli
+    mov si, msg
+    mov ah, 0x0E
+
+loop:
+    jmp loop
+
+halt: hlt
+msg: db "Hello, Welcome to BarehOS!", 0
+times 510 - ($ - $$) db 0
+dw 0xAA55
